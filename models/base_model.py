4fc9dfd8e83b7f127916509d1a0ff410fa00bf9b
rodgerswisdom <trulyhawona@gmail.com>
2024-06-17 13:16:34 +0300

    feat: __str__, to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,25 @@ class BaseModel():
     classes to inherit from
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         self.id = uuid.uuid4
         self.updated_at = datetime.now()
         self.created_at = datetime.now()
+
+    def __str__(self):
+
+        """Returns a string representation of the instance."""
+
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the public instance attribute `updated_at` with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance's `__dict__`."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
